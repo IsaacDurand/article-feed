da00a4875fb4693e12870d3fc699e1aa2c8e5a4d
Isaac Durand <isaacdurand@Isaacs-MacBook-Pro.local>
2017-05-14 14:31:38 -0700

    Render Provider component

--- client/index.js
+++ client/index.js
@@ -1,1 +1,13 @@
-console.log('Connected');
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import components
+import {Provider} from 'react-redux';
+import store from './redux/store';
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <h1>Article Feed</h1>
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ client/redux/reducer.js
@@ -0,0 +1,9 @@
+let initial = {
+  articles: []
+};
+
+function reducer(state = initial, action) {
+  return state;
+}
+
+export default reducer;
